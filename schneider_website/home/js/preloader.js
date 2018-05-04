c20749e339f32104bdab4482c86577c2fa5cf8c4
F-Schneider <s4fabi03@web.de>
2018-05-04 22:39:28 +0200

    upload_56

--- schneider_website/home/js/preloader.js
+++ schneider_website/home/js/preloader.js
@@ -1,6 +1,11 @@
 $(".content").hide()
 $("#enter").hide()
+$(".loading_animation").hide();
+
+var animation_timeout;
+
 $(window).on("load", () => {
+
   $("#enter").addClass("enter_animation_arrive")
   $("#enter").fadeIn()
   $("#enter").on("click", function animating() {
@@ -8,6 +13,12 @@ $(window).on("load", () => {
     $(".outer_ellipse_thin").addClass("ellipse_animation_leave");
     $(".preloader_container").slideToggle(2000);
     $(".content").show();
+    animation_timeout = setTimeout(timeout, 4000);
+    function timeout() {
+    }
+    $(".loading_animation").addClass("append_buttons")
+    animation_timeout;
+    $(".loading_animation").show()
 
   })
 })
