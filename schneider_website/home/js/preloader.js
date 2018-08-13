7f336c924a5ab13ac9f5a3473491ffcc10b33b8d
F-Schneider <s4fabi03@web.de>
2018-08-13 21:45:34 +0200

    upload_72

--- schneider_website/home/js/preloader.js
+++ schneider_website/home/js/preloader.js
@@ -8,6 +8,7 @@ $(window).on("load", () => {
   $("#enter").addClass("enter_animation_arrive")
   $("#enter").fadeIn()
   $("#enter").on("click", function animating() {
+    $(".content").show();
     $("#enter").addClass("enter_animation_leave");
     $(".outer_ellipse_thin").addClass("ellipse_animation_leave");
     $(".preloader_container").addClass("preloader_leave");
