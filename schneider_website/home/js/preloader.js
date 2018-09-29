c9b24fe9a98157d9416bfcc0557d3936ae7178af
F-Schneider <s4fabi03@web.de>
2018-09-29 11:05:00 +0200

    upload_73
    transition from preloader to content

--- schneider_website/home/js/preloader.js
+++ schneider_website/home/js/preloader.js
@@ -1,15 +1,13 @@
 $("#enter").hide()
-$(".content").hide();
+$(".loading_animation").hide();
 
-var animation_timeout;
 $(window).on("load", () => {
   $("#enter").addClass("enter_animation_arrive")
   $("#enter").fadeIn()
   $("#enter").on("click", function animating() {
-    $(".content").show();
     $("#enter").addClass("enter_animation_leave");
     $(".outer_ellipse_thin").addClass("ellipse_animation_leave");
-    $(".preloader_container").addClass("preloader_container_wait");
-    $(".preloader_container").addClass("preloader_container_end");
+    setTimeout(function() {$(".preloader_container").slideUp(1400)}, 700);
+    setTimeout(function() {$(".loading_animation").fadeIn(1000)}, 2000);
   })
 })
