fb4afe911d13e4a8537800ec706c420e7c687f18
F-Schneider <s4fabi03@web.de>
2019-02-16 19:21:36 +0100

    upload_102

--- schneider_website/home/js/home_jquery.js
+++ schneider_website/home/js/home_jquery.js
@@ -2,10 +2,10 @@
 alert("upload_100");
 
 $("html").css({
-  "height": "calc(100vh + 71px)";
+  "height": "calc(100vh + 71px)",
 })
 $("body").css({
-  "height": "calc(100vh + 71px)";
+  "height": "calc(100vh + 71px)",
 })
 var animating = false;
 var burger_icon = $("#burger_icon");
